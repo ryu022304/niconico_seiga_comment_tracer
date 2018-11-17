59478b00c0677241fe6f2adf5cd4dd02d8668f33
ryu022304 <ryu022304@gmail.com>
2018-11-18 02:48:51 +0900

    [update]対象コメントの矢印部分にリンクを付与。リンク先はまだ

--- src/content_script.js
+++ src/content_script.js
@@ -9,32 +9,60 @@ port.onMessage.addListener(function(response) {
   console.log("receive");
   var datas = response.datas;
   var comment_list = datas['comment_list']
-  console.log(comment_list);
-  addLinkToArrow(comment_list);
+  //console.log(comment_list);
+  var arrow_list = getArrowComment(comment_list);
+  addLinkToArrow(arrow_list);
 });
 
-function addLinkToArrow(com_list){
-  displayed_com_list = com_list.slice(-10);
-
+// 矢印付きコメントの取得
+// @param com_list 全コメント情報のリスト
+// @ret   arrow_com_list 矢印付きコメントのidと参照先コメントのJSONリスト
+function getArrowComment(com_list){
+  var arrow_com_list = [];
   for(var i in com_list){
     var comment = com_list[i]['text'];
     var id = com_list[i]['id'];
     // 「↑数字」「↑*数字」「↑繰り返し」にマッチする
-    var match = comment.match(/^(↑+)(\**)([0-9]*)/);
+    // match[0]:マッチ全体、match[1]:矢印、match[2]:繰り返し数
+    var match = comment.match(/^(↑+)\**([0-9]*)/);
     if(match != null){
       arrow_len = match[1].length;
-      repeat_num = match[3]-0;
+      repeat_num = match[2]-0;
 
       if (arrow_len < repeat_num) {
         res_comment = com_list[i - repeat_num]['text']
+        arrow_com_list.push({id:id,resid:com_list[i - repeat_num]['id'],arrow:match[0]});
         console.log('対象コメ：'+res_comment);
       }
       else{
         res_comment = com_list[i - arrow_len]['text']
+        arrow_com_list.push({id:id,resid:com_list[i - arrow_len]['id'],arrow:match[0]});
         console.log('対象コメ：'+res_comment);
       }
       //console.log(match);
       console.log(comment);
+    }
+  }
+  return arrow_com_list;
+}
+
+// 対象のコメントの矢印部分に参照先のリンクを付ける
+// @param a_list 矢印付きコメントのidと参照先コメントのJSONリスト
+function addLinkToArrow(a_list){
+  console.log(a_list);
+  var ul_elements = document.getElementsByClassName("comment_info");
+
+  for(var i in ul_elements){
+    var id = ul_elements[i]['children'].item(2).innerText.match(/[0-9]+/)[0];
+    var text = ul_elements[i]['children'].item(4).innerText;
+
+    for(var j in a_list){
+      if(a_list[j].id == id){
+        var match = text.match(/^(↑+)\**([0-9]*)(.*)/);
+        ul_elements[i]['children'].item(4).innerHTML =
+        '<a href=""><font color="red">'+match[1]+match[2]
+        +'</font></a>'+match[3];
+      }
     }
   }
 }
