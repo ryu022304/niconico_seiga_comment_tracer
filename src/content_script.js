aa5fdf8e0a311dd06a1120de0ec1c07683c60f90
ryu022304 <ryu022304@gmail.com>
2018-11-18 20:53:14 +0900

    [fix]↑×数字にも対応

--- src/content_script.js
+++ src/content_script.js
@@ -20,9 +20,13 @@ function getArrowComment(com_list){
   for(var i in com_list){
     var comment = com_list[i]['text'];
     var id = com_list[i]['id'];
-    // 「↑数字」「↑*数字」「↑繰り返し」にマッチする
+    //コメント中の全角英数字を半角に変換
+    comment = comment.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+        return String.fromCharCode(s.charCodeAt(0) - 65248);
+    });
+    // 「↑数字」「↑*数字」「↑×数字」「↑繰り返し」にマッチする
     // match[0]:マッチ全体、match[1]:矢印、match[2]:繰り返し数
-    var match = comment.match(/^(↑+)\**([0-9]*)/);
+    var match = comment.match(/^(↑+)[\*×]*([0-9]*)/);
     if(match != null){
       arrow_len = match[1].length;
       repeat_num = match[2]-0;
@@ -30,7 +34,7 @@ function getArrowComment(com_list){
       if(repeat_num > com_list.length){
         repeat_num = 0;
       }
-      //「↑10」「↑*10」とかの場合
+      //「↑10」「↑*10」「↑×10」とかの場合
       if (arrow_len < repeat_num) {
         res_comment = com_list[i - repeat_num]['text']
         arrow_com_list.push({id:id,res:com_list[i - repeat_num]['text'],arrow:match[0]});
@@ -49,21 +53,27 @@ function getArrowComment(com_list){
 // リンクをオーバーマウスで参照先のコメントが吹き出しで表示される
 // @param a_list 矢印付きコメントのidと参照先コメントのJSONリスト
 function addLinkToArrow(a_list){
+  // HTML上のコメント欄の要素を取得
   var ul_elements = document.getElementsByClassName("comment_info");
 
   for(var i in ul_elements){
+    // コメントID
     var id = ul_elements[i]['children'].item(2).innerText.match(/[0-9]+/)[0];
+    // コメント文
     var text = ul_elements[i]['children'].item(4).innerText;
-
+    //コメント中の全角英数字を半角に変換
+    text = text.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+        return String.fromCharCode(s.charCodeAt(0) - 65248);
+    });
     for(var j in a_list){
       if(a_list[j].id == id){
         var res_com = a_list[j].res;
-        var match = text.match(/^(↑+)\**([0-9]*)(.*)/);
+        var match = text.match(/^(↑+)([\*×]*)([0-9]*)(.*)/);
         ul_elements[i]['children'].item(4).innerHTML =
         '<div class="cp_tooltip"><a href="#"><font color="red">'
-        +match[1]+match[2]
+        +match[1]+match[2]+match[3]
         +'</font></a><span class="cp_tooltiptext">'
-        +res_com+'</span></div>'+match[3];
+        +res_com+'</span></div>'+match[4];
       }
     }
   }
