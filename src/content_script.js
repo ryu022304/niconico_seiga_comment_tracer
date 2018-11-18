fc86ffc3c2b1ecf9a3f0a6597af0ce8b766c6528
ryu022304 <ryu022304@gmail.com>
2018-11-18 17:36:48 +0900

    [fix]矢印パターンの場合分け追加

--- src/content_script.js
+++ src/content_script.js
@@ -1,15 +1,12 @@
 
 var url = location.href ;
 seiga_id = url.split('/')[4]
-//console.log(seiga_id);
 
 var port = chrome.runtime.connect({name: "test"});
 port.postMessage({id: seiga_id});
 port.onMessage.addListener(function(response) {
-  //console.log("receive");
   var datas = response.datas;
   var comment_list = datas['comment_list']
-  //console.log(comment_list);
   var arrow_list = getArrowComment(comment_list);
   addLinkToArrow(arrow_list);
 });
@@ -28,19 +25,20 @@ function getArrowComment(com_list){
     if(match != null){
       arrow_len = match[1].length;
       repeat_num = match[2]-0;
-
+      // 「↑64の世代は〜」「↑100年後は〜」とかの例外処理
+      if(repeat_num > com_list.length){
+        repeat_num = 0;
+      }
+      //「↑10」「↑*10」とかの場合
       if (arrow_len < repeat_num) {
         res_comment = com_list[i - repeat_num]['text']
         arrow_com_list.push({id:id,res:com_list[i - repeat_num]['text'],arrow:match[0]});
-        //console.log('対象コメ：'+res_comment);
       }
+      // 「↑↑↑」とかの場合
       else{
         res_comment = com_list[i - arrow_len]['text']
         arrow_com_list.push({id:id,res:com_list[i - arrow_len]['text'],arrow:match[0]});
-        //console.log('対象コメ：'+res_comment);
       }
-      //console.log(match);
-      //console.log(comment);
     }
   }
   return arrow_com_list;
@@ -60,7 +58,6 @@ function addLinkToArrow(a_list){
     for(var j in a_list){
       if(a_list[j].id == id){
         var res_com = a_list[j].res;
-        //console.log(res_com);
         var match = text.match(/^(↑+)\**([0-9]*)(.*)/);
         ul_elements[i]['children'].item(4).innerHTML =
         '<div class="cp_tooltip"><a href="#"><font color="red">'
